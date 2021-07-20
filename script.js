17d6b0ab57b8b882d1e8f523c0aeb6c2e5dc1fb2
RAUL R. P. SILVA <raullssilva@gmail.com>
2021-07-20 19:32:58 -0300

    tenta tiva
    limpar

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-alert('Olááá...ao digitar um numero de 1 ao 20, voce uma verá foto da Júlia, com idade proporcional. Então.....Relembre um momento especial!')
-alert('Olááá...ao digitar um numero de "1"(1925) ao "20"(2005), voce uma verá foto da Júlia, com idade proporcional. Então.....relembre um momento especial com ela...!!!')
+alert('Olááá...ao digitar um numero de 1 ao 20, voce uma verá fotos da Júlia, com idade proporcional a 1935 até 2005. Então.....Relembre um momento especial!')
