ee8ec15a71f272662a15b589eb91560e235b32a9
RAUL R. P. SILVA <raullssilva@gmail.com>
2021-08-01 15:40:29 -0300

    lembranças
    nome e posicão

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 alert('Olááá...ao digitar um numero de 1 ao 20, voce uma verá fotos da Júlia, com idade proporcional a 1935 até 2005. Então.....Relembre um momento especial!')
+function carregar(){
+    var msg= window.document.getElementById('msg') 
+    var imagem= window.document.getElementById('imagem')
+    
+
+}
